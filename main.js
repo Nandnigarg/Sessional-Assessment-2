42b2640baa9d7b2d268063fe50dd871a378d2812
Nandnigarg <gargnandni64@gmail.com>
2022-08-11 19:46:25 +0530

    Final Commit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.querySelector('#button').addEventListener('click', () => {
     let val = document.querySelector('#sinput').value
-    let comp = val-1
+    let comp = val - 1
     /* AJAX */
     const xhr = new XMLHttpRequest()
     let url = 'https://jsonplaceholder.typicode.com/users?utm_source=Mailerlite&utm_medium=E-mail&utm_campaign=Test%20Series&utm_term=2022-08-09'
@@ -11,9 +11,8 @@ document.querySelector('#button').addEventListener('click', () => {
         let output = ``
         if (xhr.status === 200 && xhr.readyState === 4) {
             const response = JSON.parse(xhr.responseText)
-            console.log(response)
             for (let i = 0; i < response.length; i++) {
-                if(i === comp){
+                if (i === comp) {
                     output = `
                         <div>
                             <table style="color:white;">
@@ -37,12 +36,31 @@ document.querySelector('#button').addEventListener('click', () => {
                                     <td style="padding:5px 20px"><h4>Website: </h4></td>
                                     <td style="padding:5px 20px"><h4> ${response[i].website}</h4><td>
                                 </tr>
+                                <tr>
+                                    <td style="padding:5px 20px"><h4>Street: </h4></td>
+                                    <td style="padding:5px 20px"><h4> ${response[i].address.street}</h4><td>
+                                </tr>
+                                <tr>
+                                    <td style="padding:5px 20px"><h4>Suite: </h4></td>
+                                    <td style="padding:5px 20px"><h4> ${response[i].address.suite}</h4><td>
+                                </tr>
+                                <tr>
+                                    <td style="padding:5px 20px"><h4>City: </h4></td>
+                                    <td style="padding:5px 20px"><h4> ${response[i].address.city}</h4><td>
+                                </tr>
+                                <tr>
+                                    <td style="padding:5px 20px"><h4>Zipcode: </h4></td>
+                                    <td style="padding:5px 20px"><h4> ${response[i].address.zipcode}</h4><td>
+                                </tr>
+                                <tr>
+                                    <td style="padding:5px 20px"><h4>Company: </h4></td>
+                                    <td style="padding:5px 20px"><h4> ${response[i].company.name}</h4><td>
+                                </tr>
                             </table>
                         </div>
                     `
                 }
             }
-            console.log(output)
             document.querySelector('#data').innerHTML = output
         }
     }
@@ -54,14 +72,29 @@ document.querySelector('#button').addEventListener('click', () => {
 document.querySelector('#pblog').addEventListener('click', (e) => {
     e.preventDefault()
     let name = document.querySelector('#pval').value
+    let fname = '-'
+    let lname = fname + name
     let head = document.querySelector('#phead').value
     let para = document.querySelector('#pmess').value
 
-    const div = document.createElement('div')
-    div.setAttribute('class', 'col-lg-6')
+    const odiv = document.createElement('div')
+    odiv.setAttribute('class', 'col-lg-6')
+
     const heading = document.createElement('h1')
     heading.setAttribute('class', 'bhead')
+    heading.setAttribute('class', 'text text-primary')
     heading.textContent = head
-    div.innerHTML = heading
-    console.log(div)
+
+    const title = document.createElement('i')
+    title.setAttribute('class', 'text text-secondary')
+    title.textContent = lname
+
+    const paragraph = document.createElement('p')
+    paragraph.textContent = para
+
+    odiv.appendChild(heading)
+    odiv.appendChild(title)
+    odiv.appendChild(paragraph)
+
+    document.querySelector('#brow').appendChild(odiv)
 })
